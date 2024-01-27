0b913d7aea9b7ff3a397e7e412db1688c18ff527
lovestudylong <87305015+lovestudylong@users.noreply.github.com>
2024-01-27 14:29:00 +0800

    实现监控图像的上传，简单美化了html页面

---
+++ project1.27/monitor/pthread_client_request.c
@@ -0,0 +1,54 @@
+/*
+ *  monitor/pthread_client_request.c
+ *
+ *  (C) 2024  Chen Zichao
+ */
+
+#include "common.h"
+
+struct msg msgbuf;
+
+#define MSGLEN (sizeof(struct msg) - sizeof(long))
+
+void *pthread_client_request(void *arg)
+{
+    msg_key = ftok("/tmp", 100);
+    if (msg_key < 0) {
+        perror("ftok");
+        exit(-1);
+    }
+
+    msgid = msgget(msg_key, IPC_CREAT | 0666);
+    if (msgid < 0) {
+        perror("msgget");
+        exit(-1);
+    }
+
+    printf("pthread_client_request is ready\n");
+
+    while (1) {
+        bzero(&msgbuf, sizeof(msgbuf));
+        msgrcv(msgid, &msgbuf, MSGLEN, 0, 0);
+        usleep(5);
+        printf("receive msgtype = %ld, text = %#x\n", msgbuf.msgtype, msgbuf.text[0]);
+
+        switch (msgbuf.msgtype) {
+            case 1L: // LED
+                pthread_mutex_lock(&mutex_led);
+                printf("led request\n");
+                cmd_led = msgbuf.text[0];
+                pthread_mutex_unlock(&mutex_led);
+                pthread_cond_signal(&cond_led);
+                break;
+            case 2L: // BUZZER
+                pthread_mutex_lock(&mutex_pwm);
+                printf("buzzer request\n");
+                cmd_pwm = msgbuf.text[0];
+                pthread_mutex_unlock(&mutex_pwm);
+                pthread_cond_signal(&cond_pwm);
+                break;
+            default:
+                break;
+        }
+    }
+}
---
+++ project1.27/web/cgi_refresh.c
@@ -0,0 +1,100 @@
+/*
+ *  web/cgi_refresh.c
+ *
+ *  (C) 2024  Chen Zichao
+ */
+
+#include <semaphore.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/shm.h>
+#include "../monitor/sem.h"
+
+#define SEM_NUM 1
+#define MEMORY_SIZE 1024
+
+struct monitor_info {
+    unsigned int adc;
+    short gyrox;
+    short gyroy;
+    short gyroz;
+    short aacx;
+    short aacy;
+    short aacz;
+    short temp;
+};
+
+int main(int argc, const char *argv[])
+{
+    key_t shm_key, sem_key;
+    int shmid, semid;
+    struct monitor_info *env_data_buf;
+
+    sem_key = ftok("/tmp", 100);
+    if (sem_key < 0) {
+        perror("ftok");
+        exit(-1);
+    }
+
+    semid = semget(sem_key, SEM_NUM, IPC_CREAT | 0666);
+    if (semid < 0) {
+        perror("semget");
+        exit(-1);
+    }
+
+    shm_key = ftok("/tmp", 100);
+    if (shm_key < 0) {
+        perror("ftok");
+        exit(-1);
+    }
+
+    shmid = shmget(shm_key, MEMORY_SIZE, IPC_CREAT | 0666);
+    if (shmid < 0) {
+        perror("shmget");
+        exit(-1);
+    }
+
+    env_data_buf = (struct monitor_info *)shmat(shmid, NULL, 0);
+    if (env_data_buf < 0) {
+        perror("shmat");
+        exit(-1);
+    }
+
+    sem_p(semid, 0);
+
+    printf("Content-type: text/html;charset=utf-8\n\n");
+    
+	printf("<head><meta http-equiv=\"refresh\" content=\"1\"></style> </head>");
+	printf("<html>\n");
+    printf("<script> \
+    function show(){var date = new Date(); \
+    var now = \"\"; \
+    now = date.getFullYear()+\"年\"; \
+    now = now + (date.getMonth()+1)+\"月\"; \
+    now = now + date.getDate()+\"日\"; \
+    now = now + date.getHours()+\"时\"; \
+    now = now + date.getMinutes()+\"分\"; \
+    now = now + date.getSeconds()+\"秒\"; \
+    document.getElementById(\"nowDiv\").innerHTML = now; \
+    setTimeout(\"show()\",1000);} </script>\n");
+    printf("<h3 align=center><body onload=\"show()\"> <div id=\"nowDiv\"></div></h3>\n");
+
+    printf("<h4>ADC数据：</h4>\n");
+    printf("<p>adc:%dmv</p>\n", env_data_buf->adc);
+    printf("<h4>MPU6050数据：</h4>\n");
+    printf("<p>gyrox:%d</p>\n", env_data_buf->gyrox);
+    printf("<p>gyroy:%d</p>\n", env_data_buf->gyroy);
+    printf("<p>gyroz:%d</p>\n", env_data_buf->gyroz);
+    printf("<p>aacx:%d</p>\n", env_data_buf->aacx);
+    printf("<p>aacy:%d</p>\n", env_data_buf->aacy);
+    printf("<p>aacz:%d</p>\n", env_data_buf->aacz);
+    printf("<p>temperature:%d°C</p>\n", env_data_buf->temp);
+
+	printf("</html>\n");
+
+    sem_v(semid, 0);
+
+    return 0;
+}
